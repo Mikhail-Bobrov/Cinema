e976d2f797087112ec6713091a8a65cf2fc44207
Mikhail <bobrov.mikhail94@gmail.com>
2021-08-21 16:31:49 +0300

    Cinema before i will make last piece

---
+++ Topics/Array exceptions/Calculate Square/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+class FixingExceptions {
+
+    public static void calculateSquare(int[] array, int index) {
+       try {
+           System.out.println(array[index] * array[index]);
+       }
+       catch (Exception e){
+           System.out.println("Exception!");
+       }
+       }}
